d3db6c9cce8a8ed2084511b87e09c9d96db5e0cb
Ezequiel Parziale <eparziale@gmail.com>
2023-05-25 12:47:15 -0300

    :rocket: update: add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express from 'express'
+import morgan from 'morgan'
 import healthRouter from './routes/health.router'
 
 const app = express()
 
+// Middlewares
+app.use(morgan('dev'))
 app.use(express.json())
 
+// Routes
 app.get('/ping', (_, res) => {
   console.log('Process ping request')
   res.send('pong')
 })
 
-// Routes
 app.use('/health', healthRouter)
 
 export default app
