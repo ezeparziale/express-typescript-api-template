078ecdf4692ee9a04164d340f5439d35082b16d3
Ezequiel Parziale <eparziale@gmail.com>
2023-05-26 14:12:55 -0300

    :rocket: update: add validate schema

--- src/routes/post.router.ts
+++ src/routes/post.router.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express'
 import postController from '../controllers/post.controller'
 import { requireAuth } from '../middlewares/authenticate.middleware'
+import { validateSchema } from '../middlewares/validateSchema.middleware'
+import { postCreateSchema, postEditSchema } from '../schemas/post.schema'
 
 const router = Router()
 
-router.post('/', requireAuth, postController.createNewPost)
+router.post(
+  '/',
+  requireAuth,
+  validateSchema(postCreateSchema),
+  postController.createNewPost,
+)
 router.get('/', requireAuth, postController.getAllPosts)
 router.get('/:postId', requireAuth, postController.getSinglePost)
-router.put('/:postId', requireAuth, postController.updateOnePost)
+router.put(
+  '/:postId',
+  requireAuth,
+  validateSchema(postEditSchema),
+  postController.updateOnePost,
+)
 router.delete('/:postId', requireAuth, postController.deleteOnePost)
 
 export default router
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express'
 import userController from '../controllers/user.controller'
 import { requireAuth } from '../middlewares/authenticate.middleware'
+import { validateSchema } from '../middlewares/validateSchema.middleware'
+import { userCreateSchema } from '../schemas/user.schema'
 
 const router = Router()
 
-router.post('/', requireAuth, userController.createNewUser)
+router.post(
+  '/',
+  requireAuth,
+  validateSchema(userCreateSchema),
+  userController.createNewUser,
+)
 router.get('/', requireAuth, userController.getAllUser)
 router.get('/me', requireAuth, userController.getMe)
 router.get('/:userId', requireAuth, userController.getOneUser)
-router.put('/:userId', requireAuth, userController.updateOneUser)
+router.put(
+  '/:userId',
+  requireAuth,
+  validateSchema(userCreateSchema),
+  userController.updateOneUser,
+)
 router.delete('/:userId', requireAuth, userController.deleteOneUser)
 
 export default router
