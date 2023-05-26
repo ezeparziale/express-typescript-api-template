1c79d4afb6e000ab586287d606bebdbc059918f1
Ezequiel Parziale <eparziale@gmail.com>
2023-05-26 13:45:53 -0300

    :wrench: update: add new env vars

--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -3,7 +3,17 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // Server
-const PORT: string | number = process.env.PORT || 3000
-const BACKEND_CORS_ORIGINS: string = process.env.BACKEND_CORS_ORIGINS || '*'
+export const PORT: number = Number(process.env.PORT) || 3000
+export const BACKEND_CORS_ORIGINS: string = process.env.BACKEND_CORS_ORIGINS || '*'
 
-export { PORT, BACKEND_CORS_ORIGINS }
+// Jwt
+export const SECRET_KEY: string = process.env.SECRET_KEY || ''
+export const ALGORITHM: string = process.env.BACKEND_CORS_ORIGINS || 'HS256'
+export const ACCESS_TOKEN_EXPIRE_MINUTES: number = Number(process.env.PORT) || 300
+
+// Postgres Database
+export const POSTGRES_HOSTNAME: string = process.env.POSTGRES_HOSTNAME || ''
+export const POSTGRES_PORT: number = Number(process.env.POSTGRES_PORT) || 5432
+export const POSTGRES_PASSWORD: string = process.env.POSTGRES_PASSWORD || ''
+export const POSTGRES_USER: string = process.env.POSTGRES_USER || ''
+export const POSTGRES_DB: string = process.env.POSTGRES_DB || ''
