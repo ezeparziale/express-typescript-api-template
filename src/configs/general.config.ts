7ee6abd8a0280e23adcf81a6f23451803076f08d
Ezequiel Parziale <eparziale@gmail.com>
2023-05-26 22:01:27 -0300

    :bug: fix: env vars config

--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -8,8 +8,9 @@ export const BACKEND_CORS_ORIGINS: string = process.env.BACKEND_CORS_ORIGINS || 
 
 // Jwt
 export const SECRET_KEY: string = process.env.SECRET_KEY || ''
-export const ALGORITHM: string = process.env.BACKEND_CORS_ORIGINS || 'HS256'
-export const ACCESS_TOKEN_EXPIRE_MINUTES: number = Number(process.env.PORT) || 300
+export const ALGORITHM: string = process.env.ALGORITHM || 'HS256'
+export const ACCESS_TOKEN_EXPIRE_MINUTES: number =
+  Number(process.env.ACCESS_TOKEN_EXPIRE_MINUTES) || 30
 
 // Postgres Database
 export const POSTGRES_HOSTNAME: string = process.env.POSTGRES_HOSTNAME || ''
